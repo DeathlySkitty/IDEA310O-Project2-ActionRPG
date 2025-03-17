f8d707ee0dc8c4bd006f7ab6510aeb5715b61ff4
Skitty <112578541+DeathlySkitty@users.noreply.github.com>
2025-03-17 12:39:35 -0600

    Tutorial Edits

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -4,25 +4,72 @@ using CreatorKitCode;
 public class SpawnerSample : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
+    LootAngle myLootAngle = new LootAngle(45);
 
     void Start()
     {
-        int angle = 15;
-        Vector3 spawnPosition = transform.position;
+        //every call will advance the angle!
+        PotionSpawner(myLootAngle.NextAngle());
+        PotionSpawner(myLootAngle.NextAngle());
+        PotionSpawner(myLootAngle.NextAngle());
+        PotionSpawner(myLootAngle.NextAngle());
+
+        // PotionSpawner(0);
+        // PotionSpawner(45);
+        // PotionSpawner(90);
+        // PotionSpawner(135);
+
+        // int angle = 15;
+        // int radius = 5;
+        // Vector3 spawnPosition = transform.position;
+
+        // Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        // spawnPosition = transform.position + direction * radius;
+        // Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+        // angle = 55;
+        // direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        // spawnPosition = transform.position + direction * radius;
+        // Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+        // angle = 95;
+        // direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        // spawnPosition = transform.position + direction * radius;
+        // Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+        // angle = 105;
+        // direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        // spawnPosition = transform.position + direction * radius;
+        // Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    }
+
+    void PotionSpawner(int angle) 
+    {
+        int radius = 5;
 
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        Vector3 spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    }
+}
 
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+public class LootAngle 
+{
+    int angle; 
+    int step; 
 
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    public LootAngle(int increment)
+    { 
+        step = increment;
+        angle = 0;
+    }
+
+    public int NextAngle() 
+    {
+        int currentAngle = angle;
+        angle = Helpers.WrapAngle(angle + step);
+
+        return currentAngle;
     }
 }
 
